67516b9e242b06b2c382186408d67c3831626828
StevenLopez180805 <stevenlopez180805@gmail.com>
2025-10-15 00:33:17 -0500

    Adicion de metodos para editar y eliminar libros

--- src/libros/libros.controller.ts
+++ src/libros/libros.controller.ts
@@ -5,6 +5,7 @@ import { Roles } from '../auth/roles.decorator';
 import { RolesGuard } from '../auth/roles.guard';
 import { LibrosService } from './libros.service';
 import { CreateLibroDto } from './dto/create-libro.dto';
+import { UpdateLibroDto } from './dto/update-libro.dto';
 
 
 @Controller('libros')
@@ -24,5 +25,25 @@ export class LibrosController {
   findAll() {
     return this.librosService.findAll();
   }
-  
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('bibliotecario', 'estudiante')
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.librosService.findOne(+id);
+  }
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('bibliotecario')
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateLibroDto: UpdateLibroDto) {
+    return this.librosService.update(+id, updateLibroDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('bibliotecario')
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.librosService.remove(+id);
+  }
 }
